84d941903c3f28c35a853a98fd356549dae85e0b
Jae You <jyou88@gmail.com>
2022-12-07 14:28:10 -0600

    updated post jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Posts from './components/Posts.jsx';
 
 function App() {
-  return <div className="App">Stranger's Things</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">Stranger's Things</div>
+      <Posts />
+    </BrowserRouter>)
 }
 
 export default App;
